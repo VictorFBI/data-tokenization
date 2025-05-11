c5cf8cdb8161461239e963d2c27580408aed5464
VictorFBI <vdfrolovbukanov@edu.hse.ru>
2025-05-11 21:00:25 +0300

    do refactoring

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -5,14 +5,80 @@ import (
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 
+	"data-tokenization/handlers"
 	"data-tokenization/internal/app/rest"
+	"data-tokenization/internal/gen/contracts"
 	"data-tokenization/internal/gen/restgen"
 	"data-tokenization/internal/pkg/business"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	"context"
+	"log"
+	"math/big"
 )
 
+const (
+	DeployerPrivateKey  = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+	HardhatRPCServerURL = "http://localhost:8545"
+	BackendServerURL    = "localhost:8081"
+)
+
+func deployContracts() {
+	client, err := ethclient.Dial(HardhatRPCServerURL)
+	if err != nil {
+		panic(err)
+	}
+
+	chainID, err := client.ChainID(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	privateKey, err := crypto.HexToECDSA(DeployerPrivateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+	if err != nil {
+		panic(err)
+	}
+
+	address, _, _, err := contracts.DeployStorage(auth, client)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Contract deployed at address: ", address.Hex())
+
+	instance, err := contracts.NewStorage(address, client)
+	if err != nil {
+		panic(err)
+	}
+
+	// nolint
+	instance.Store(auth, big.NewInt(42))
+
+	number, _ := instance.Retrieve(&bind.CallOpts{})
+
+	log.Println("Number: ", number)
+}
+
 func main() {
+	if err := handlers.InitIPFS(); err != nil {
+		panic(err)
+	}
+
+	deployContracts()
+
 	// Create a Gin router
 	router := gin.Default()
+
+	// Setting max file size (8 MB)
+	router.MaxMultipartMemory = 8 << 20
 
 	// Register the handler
 	restgen.RegisterHandlers(router, rest.NewTokenHandler(business.NewService()))
@@ -25,7 +91,7 @@ func main() {
 	))
 
 	// 3. Отдача исходного YAML
-	router.StaticFile("/api.yaml", "./api/rest/api.yaml")
+	router.StaticFile("/api.yaml", "../../api/rest/api.yaml")
 
 	// Start the server on port 8080
 	if err := router.Run(":8080"); err != nil {
